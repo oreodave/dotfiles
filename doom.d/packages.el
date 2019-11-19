9a427c8f6e72782dc691e5a9a2f5e5ba25f4aa7f
oreodave <aryadevchavali1@gmail.com>
2019-11-19 12:02:30 +0000

    +comments for each package reference

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -6,11 +6,11 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 ; General
-(package! elcord)
-(package! wttrin)
+(package! elcord) ; to flex
+(package! wttrin) ; weather in emacs? yes please
 ; Coding
-(package! counsel-etags)
-(package! py-yapf)
+(package! counsel-etags) ; tags are cool
+(package! py-yapf) ; formatting
 ; Arduino
-(package! arduino-mode)
-(package! company-arduino)
+(package! arduino-mode) ; mandatory, though I might make my own module for this
+(package! company-arduino) ; intellisense EVERYTHING
