d7d642bdd94aca6232bfb80dd27c6897e7ef5749
AChavali <aryadevchavali1@gmail.com>
2020-01-17 02:30:09 +0000

    ~packages.el -> packages.org
    Using an org file for packages now! Explaining my choices and stuff.
    packages.el loads bin/packages.el now

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -1,18 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
-; General
-(package! elcord) ; to flex
-(package! wttrin) ; weather in emacs? yes please
-(package! wakatime-mode) ; wakatime
-(package! powerthesaurus)
-; Coding
-(package! counsel-etags) ; tags are cool
-(package! py-yapf) ; formatting
-; Arduino
-(package! arduino-mode) ; mandatory, though I might make my own module for this
-(package! company-arduino) ; intellisense EVERYTHING
+(load! "bin/packages.el")
