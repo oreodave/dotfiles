9ced0a3677eb55120e71a4c1bf652ea24aadf272
odave <aryadevchavali1@gmail.com>
2020-03-17 17:03:10 +0000

    ~gentemplate.org -> gentemplate module
    Gentemplate is one of those things that just makes sense as a module.
    Thus, converted it. I may convert a few more org files into full blown modules.

---
+++ doom.d/modules/private/gentemplate/config.el
@@ -0,0 +1,32 @@
+;;; private/gentemplate/config.el -*- lexical-binding: t; -*-
+
+(require 'cl)
+
+(setq +gentemplate/template-list (list "CTemplate" "CPPTemplate" "PythonTemplate" "NodeTemplate" "ArduinoTemplate" "JavaTemplate"))
+(setq +gentemplate/profile-url "https://github.com/oreodave/")
+
+(defun +gentemplate/offline ()
+  (eq (list-length (network-interface-list)) 1))
+
+(after! magit-clone
+  (defun +gentemplate/download-template (template-name dest)
+    "Download a given template via its `template-name' to the `dest' folder"
+    (magit-clone-regular (concat +gentemplate/profile-url template-name) dest nil)))
+
+(defun +gentemplate/copy-template (template-name dest)
+  "Copy a template project via it's `template-name' to a folder called `dest'"
+  (copy-directory (expand-file-name (concat "~/Code/Templates/" template-name)) dest))
+
+(after! (ivy magit-clone)
+  (defun +gentemplate/generate-template ()
+    (interactive)
+    (ivy-read
+     "Enter template: "
+     +gentemplate/template-list
+     :action
+     (lambda (template-name)
+       (let ((dir (read-directory-name "Enter directory to download to: "))
+             (offline (+gentemplate/offline)))
+         (if offline
+             (+gentemplate/copy-template template-name dir)
+           (+gentemplate/download-template template-name dir)))))))
