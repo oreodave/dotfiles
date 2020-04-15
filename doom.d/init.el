409da204024e10c09242e1c6b9195f52aac9b82e
dx <aryadevchavali1@gmail.com>
2020-04-15 04:34:22 +0100

    +put rss in config.el directly

--- doom.d/init.el
+++ doom.d/init.el
@@ -167,7 +167,7 @@
        :app
        calendar
        irc              ; how neckbeards socialize
-       (rss +org)        ; emacs as an RSS reader
+       (rss)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
