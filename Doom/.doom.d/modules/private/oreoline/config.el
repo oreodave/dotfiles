6c262a85bc5b806797de340883becc7796334823
dx <aryadevchavali1@gmail.com>
2020-05-31 15:38:09 +0100

    ~formatting oreoline/config.el

--- Doom/.doom.d/modules/private/oreoline/config.el
+++ Doom/.doom.d/modules/private/oreoline/config.el
@@ -3,7 +3,7 @@
 (use-package! telephone-line
   :hook (after-init . telephone-line-mode)
   :init
-                                        ; Faces
+  ;; Faces
   (defface my-accent-dark    '((t (:foreground "Black" :background "Cadet Blue"))) "")
   (defface my-evil-dark      '((t (:foreground "White" :background "Black"))) "")
   (defface my-accent-light   '((t (:foreground "black" :background "Light Slate Grey"))) "")
@@ -11,49 +11,49 @@
 
   ;; Set telephone line faces
   (setq telephone-line-faces
-    '((evil . (my-evil-dark . my-evil-dark))
-       (modal . telephone-line-modal-face)
-       (ryo . telephone-line-ryo-modal-face)
-       (accent . (my-accent-dark . telephone-line-accent-inactive))
-       (nil mode-line . mode-line-inactive)))
+        '((evil . (my-evil-dark . my-evil-dark))
+          (modal . telephone-line-modal-face)
+          (ryo . telephone-line-ryo-modal-face)
+          (accent . (my-accent-dark . telephone-line-accent-inactive))
+          (nil mode-line . mode-line-inactive)))
   (when (featurep! +light)
     (setq telephone-line-faces
-      '((evil . (my-evil-light . my-evil-light))
-         (modal . telephone-line-modal-face)
-         (ryo . telephone-line-ryo-modal-face)
-         (accent . (my-accent-light . telephone-line-accent-inactive))
-         (nil mode-line . mode-line-inactive))))
+          '((evil . (my-evil-light . my-evil-light))
+            (modal . telephone-line-modal-face)
+            (ryo . telephone-line-ryo-modal-face)
+            (accent . (my-accent-light . telephone-line-accent-inactive))
+            (nil mode-line . mode-line-inactive))))
 
-                                        ; Seperators
+  ;; Seperators
   (setq telephone-line-primary-left-separator    'telephone-line-abs-left
-    telephone-line-secondary-left-separator  'telephone-line-identity-hollow-left
-    telephone-line-primary-right-separator   'telephone-line-abs-right
-    telephone-line-secondary-right-separator 'telephone-line-identity-hollow-right)
+        telephone-line-secondary-left-separator  'telephone-line-identity-hollow-left
+        telephone-line-primary-right-separator   'telephone-line-abs-right
+        telephone-line-secondary-right-separator 'telephone-line-identity-hollow-right)
 
-                                        ; LSP segment
+  ;; LSP segment
   (telephone-line-defsegment +oreoline-lsp-segment ()
     (if (bound-and-true-p lsp-mode)
         (propertize "")
       (propertize "")))
 
-                                        ; LHS
+  ;; LHS
   (setq telephone-line-lhs
-    '((evil   . (telephone-line-evil-tag-segment
-                 telephone-line-buffer-modified-segment))
-       (accent . (telephone-line-vc-segment
-                   telephone-line-filesize-segment
-                   telephone-line-buffer-name-segment
-                   telephone-line-erc-modified-channels-segment
-                   telephone-line-process-segment))
-       (nil    . ())))
+        '((evil   . (telephone-line-evil-tag-segment
+                     telephone-line-buffer-modified-segment))
+          (accent . (telephone-line-vc-segment
+                     telephone-line-filesize-segment
+                     telephone-line-buffer-name-segment
+                     telephone-line-erc-modified-channels-segment
+                     telephone-line-process-segment))
+          (nil    . ())))
 
-                                        ; RHS
+  ;; RHS
   (setq telephone-line-rhs
-    '((nil    . (telephone-line-misc-info-segment))
-       (accent . (telephone-line-major-mode-segment
-                   +oreoline-lsp-segment
-                   telephone-line-flycheck-segment))
-       (evil   . (telephone-line-airline-position-segment))))
+        '((nil    . (telephone-line-misc-info-segment))
+          (accent . (telephone-line-major-mode-segment
+                     +oreoline-lsp-segment
+                     telephone-line-flycheck-segment))
+          (evil   . (telephone-line-airline-position-segment))))
   :config
   (size-indication-mode +1))
 
