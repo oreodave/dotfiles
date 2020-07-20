d8eb5ff0200857af534c5c2f105b89c854c1a8f2
dx <aryadevchavali1@gmail.com>
2020-07-20 21:08:59 +0100

    ~clean up init.el

--- Doom/.config/doom/init.el
+++ Doom/.config/doom/init.el
@@ -47,10 +47,10 @@
        zen
 
        :editor
-       evil +everywhere); come to the dark side, we have cookies
+       (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format)  ; automated prettiness
+       format  ; automated prettiness
        ;;lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
