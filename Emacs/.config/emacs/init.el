0cb72b3793de453fad20df2be36bca86d3bd3dc9
Aryadev Chavali <aryadev@aryadevchavali.com>
2020-08-11 20:10:23 +0100

    ~put straight into init.el

--- Emacs/.config/emacs/init.el
+++ Emacs/.config/emacs/init.el
@@ -1,3 +1,20 @@
+;; Straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default t)
+(straight-use-package 'use-package)
+
 ;; Load literate
 (setq user-emacs-directory "~/.config/emacs/")
 (defconst +literate/files (list "config.org"))
