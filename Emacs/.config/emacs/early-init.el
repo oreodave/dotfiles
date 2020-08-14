cbd1676781aedf79313356d90ea929029cf91d78
Aryadev Chavali <aryadev@aryadevchavali.com>
2020-08-14 23:41:38 +0100

    +early-init.el
    From Emacs 27+, early-init.el provides control over stuff earlier than
    UI load allowing for fine grained control over the load up. Here you
    setup stuff such that Emacs loads this code most eagerly.
    
    In this case, I set gc-cons-threshold to the highest value to
    aggressively load the config without care for garbage collection, as
    well as restricting standard package use. Along with that are some
    basic UI things so that I don't have to deal with them even in load
    up such as menu-bars and the alpha.
    
    As this code is not error prone at all and is loaded before init.el
    these choices allow for an easier debugging experience as well.

---
+++ Emacs/.config/emacs/early-init.el
@@ -0,0 +1,8 @@
+(setq gc-cons-threshold most-positive-fixnum
+      package-enable-at-startup nil
+      frame-inhibit-implied-resize t)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(alpha . 85) default-frame-alist)
+(advice-add #'x-apply-session-resources :override #'ignore)
