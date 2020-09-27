1469245d30e2c97ee26f120a29f421eeb9d8df73
Aryadev Chavali <aryadev@aryadevchavali.com>
2020-09-27 22:12:07 +0100

    (Emacs)+header for init.el

--- Emacs/.config/emacs/init.el
+++ Emacs/.config/emacs/init.el
@@ -1,6 +1,24 @@
-;;; init.el --- My custom init.el that starts my Emacs
-;; Author: Aryadev Chavali <aryadev@aryadevchavali.com
-;; This file is NOT part of GNU Emacs.
+;;; init.el --- My custom init.el that starts my Emacs  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Aryadev Chavali
+
+;; Author: Aryadev Chavali <aryadev@aryadevchavali.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
 ;;; Code:
 
 ;; Straight
@@ -69,3 +87,6 @@
   (require 'eglot))
 
 (setq gc-cons-threshold 20000000)
+
+(provide 'init)
+;;; init.el ends here
