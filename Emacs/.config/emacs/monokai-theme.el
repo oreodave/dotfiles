6fb42a1b53bfc0b891c0e785ad5116bb36367cb3
Aryadev Chavali <aryadev@aryadevchavali.com>
2021-02-28 23:13:54 +0000

    (Emacs)+hl-line theming for monokai theme

--- Emacs/.config/emacs/monokai-theme.el
+++ Emacs/.config/emacs/monokai-theme.el
@@ -1,5 +1,5 @@
 (deftheme monokai
-  "Created 2020-09-30.")
+  "Created 2021-02-28.")
 
 (custom-theme-set-faces
  'monokai
@@ -55,7 +55,7 @@
  '(company-tooltip-selection ((t (:background "firebrick4"))))
  '(org-quote ((t (:inherit org-block :slant italic :family "Liberation Mono"))))
  '(org-verse ((t (:inherit org-block :slant oblique :family "Liberation Mono"))))
- '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "#d6d6d4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "ADBO" :family "ibm plex mono"))))
- '(ledger-font-posting-account-face ((t nil))))
+ '(hl-line ((t (:extend t :background "gray10"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "#d6d6d4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 125 :width normal :foundry "ADBO" :family "ibm plex mono")))))
 
 (provide-theme 'monokai)
