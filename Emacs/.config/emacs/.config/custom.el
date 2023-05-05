8f4018b39fec4cf3e74955b2bcc9a6111470b462
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-05-05 20:16:43 +0100

    (Emacs)~stuff

--- Emacs/.config/emacs/.config/custom.el
+++ Emacs/.config/emacs/.config/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   (flycheck-mode . t))))
+   (flycheck-mode . t)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
