66fa84dd1000982f7de4a7a511ee2b631cc437d1
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-09-22 14:50:08 +0100

    (Emacs)~custom.el has safe values for projectile-*-cmd

--- Emacs/.config/emacs/.config/custom.el
+++ Emacs/.config/emacs/.config/custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   (flycheck-mode . t)))
+   '((projectile-project-configure-cmd . "make clean")
+     (projectile-project-test-cmd . "make test")
+     (projectile-project-run-cmd . "make run")
+     (projectile-project-compilation-cmd . "make"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
