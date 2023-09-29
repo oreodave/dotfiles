cc25dcf8121c7a0431f5fc1f27675b1229818041
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-09-29 21:40:45 +0100

    (Emacs)~more safe values

--- Emacs/.config/emacs/.config/custom.el
+++ Emacs/.config/emacs/.config/custom.el
@@ -4,9 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((+license/license-choice "MIT")
+   '((+license/license-choice . "MIT")
      (projectile-project-configure-cmd . "make clean")
      (projectile-project-test-cmd . "make test")
+     (projectile-project-test-cmd . "make run-test")
      (projectile-project-run-cmd . "make run")
      (projectile-project-compilation-cmd . "make"))))
 (custom-set-faces
