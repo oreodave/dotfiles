5a5736335aea4052203eaf1c45d38206d985bafb
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-09-29 22:40:08 +0100

    (Emacs)~little clean

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables         -*- no-byte-compile: t; -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((+license/license-choice "MIT"))))
+((nil . ((+license/license-choice . "MIT"))))
