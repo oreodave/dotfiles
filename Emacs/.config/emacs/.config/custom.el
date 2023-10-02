9e975839fe0b98649549173fe04f9191226e0657
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-10-02 05:34:23 +0100

    (Emacs)~dir-locals

--- Emacs/.config/emacs/.config/custom.el
+++ Emacs/.config/emacs/.config/custom.el
@@ -4,11 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((+license/license-choice . "MIT")
-     (projectile-project-configure-cmd . "make clean")
-     (projectile-project-test-cmd . "make test")
-     (projectile-project-test-cmd . "make run-test")
-     (projectile-project-run-cmd . "make run")
+   '((+license/license-choice            . "GPLv2")
+     (+license/license-choice            . "MIT")
+     (projectile-project-configure-cmd   . "make clean")
+     (projectile-project-test-cmd        . "make test")
+     (projectile-project-test-cmd        . "make run-test")
+     (projectile-project-run-cmd         . "make run")
      (projectile-project-compilation-cmd . "make"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
