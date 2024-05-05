41bf661eed2a7d26872dbb60b91777bb7895c2e3
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-05-06 02:36:54 +0530

    (Emacs/init)~gc-cons-threshold is set to max at start of bootup

--- Emacs/.config/emacs/init.el
+++ Emacs/.config/emacs/init.el
@@ -25,60 +25,59 @@
 ;; Before doing anything else, make gc-cons-threshold ridiculously
 ;; high.  This makes it so we have as few pauses during init as
 ;; possible.
-(setq gc-cons-threshold (* 1024 1024 1024)) ; ~1GiB
+(let ((gc-cons-threshold most-positive-fixnum))
+  ;; Straight
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+         (expand-file-name
+          "straight/repos/straight.el/bootstrap.el"
+          (or (bound-and-true-p straight-base-dir)
+             user-emacs-directory)))
+        (bootstrap-version 7))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
 
-;; Straight
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+  (setq straight-disable-native-compile nil
+        straight-use-package-by-default t
+        use-package-enable-imenu-support t
+        use-package-always-demand nil
+        use-package-always-defer nil
+        use-package-hook-name-suffix nil
+        use-package-compute-statistics t)
 
-(setq straight-disable-native-compile nil
-      straight-use-package-by-default t
-      use-package-enable-imenu-support t
-      use-package-always-demand nil
-      use-package-always-defer nil
-      use-package-hook-name-suffix nil
-      use-package-compute-statistics t)
+  (straight-use-package 'use-package)
+  (straight-use-package 'org)
+  (straight-use-package 'no-littering)
 
-(straight-use-package 'use-package)
-(straight-use-package 'org)
-(straight-use-package 'no-littering)
+  (setq no-littering-etc-directory (expand-file-name ".config/"  user-emacs-directory)
+        no-littering-var-directory (expand-file-name ".local/" user-emacs-directory)
+        custom-file (no-littering-expand-etc-file-name "custom.el"))
 
-(setq no-littering-etc-directory (expand-file-name ".config/"  user-emacs-directory)
-      no-littering-var-directory (expand-file-name ".local/" user-emacs-directory)
-      custom-file (no-littering-expand-etc-file-name "custom.el"))
-
-(load-file custom-file)
+  (load-file custom-file)
 
 ;;; Load literate
-(load-file (concat user-emacs-directory "elisp/literate.el"))
+  (load-file (concat user-emacs-directory "elisp/literate.el"))
 
-;; Compile on Emacs quit
-(add-hook
- 'kill-emacs-hook
- #'+literate/compile-config)
+  ;; Compile on Emacs quit
+  (add-hook
+   'kill-emacs-hook
+   #'+literate/compile-config)
 
-(+literate/load-config)
+  (+literate/load-config)
 
-(when (daemonp)
-  (require 'general)
-  (require 'evil)
-  (require 'notmuch)
-  (require 'company)
-  (require 'org)
-  (require 'eglot))
+  (when (daemonp)
+    (require 'general)
+    (require 'evil)
+    (require 'notmuch)
+    (require 'company)
+    (require 'org)
+    (require 'eglot)))
 
 (setq gc-cons-threshold (* 100 1024 1024) ; ~100MiB
       read-process-output-max 5242880 ; ~5MiB
