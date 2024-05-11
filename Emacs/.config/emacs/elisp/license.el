6aa3c170e93ea9a88b16816a3396778def524a01
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-05-11 13:05:41 +0530

    (Emacs/*)~clean up text, change license of elisp packages to GPLv2 strictly

--- Emacs/.config/emacs/elisp/license.el
+++ Emacs/.config/emacs/elisp/license.el
@@ -5,10 +5,9 @@
 ;; Author: Aryadev Chavali <aryadev@aryadevchavali.com>
 ;; Keywords:
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License version
+;; 2 as published by the Free Software Foundation.
 
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,9 +18,7 @@
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
 ;;
-
 ;;; Code:
 
 (defvar +license/license-choice nil)
