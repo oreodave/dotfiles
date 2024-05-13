fdef946d6555b01b33fe496cdcd6fb293ae11eab
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-05-13 17:36:40 +0530

    (Emacs/custom)~clean up some old customisations

--- Emacs/.config/emacs/.config/custom.el
+++ Emacs/.config/emacs/.config/custom.el
@@ -4,13 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((+license/license-choice            . "GPLv2")
-     (+license/license-choice            . "MIT")
-     (projectile-project-configure-cmd   . "make clean")
-     (projectile-project-test-cmd        . "make test")
-     (projectile-project-test-cmd        . "make run-test")
-     (projectile-project-run-cmd         . "make run")
-     (projectile-project-compilation-cmd . "make"))))
+   '((eval eglot-ensure)
+     (+license/license-choice . "GPLv2")
+     (+license/license-choice . "Unlicense")
+     (+license/license-choice . "MIT"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
